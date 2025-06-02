0403231de11c8f0fd4d2475e14bbfe78aeac7669
wayyytoheaven <133392697+wayyytoheaven@users.noreply.github.com>
2025-06-02 12:42:42 +0300

    Add files via upload

--- tgapp/scripts/main.js
+++ tgapp/scripts/main.js
@@ -1,28 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const userNameElement = document.getElementById('userName'); // Элемент с именем
-    const userAvatarElement = document.getElementById('userAvatar'); // Элемент с аватаром (буквой)
-
-    // Получаем имя пользователя из localStorage
+    const userNameElement = document.getElementById('userName');
+    const userAvatarElement = document.getElementById('userAvatar');
     const storedUserName = localStorage.getItem('userName');
 
     if (storedUserName) {
         userNameElement.textContent = storedUserName;
-        userAvatarElement.textContent = storedUserName.charAt(0).toUpperCase(); // Первая буква в верхнем регистре
+        userAvatarElement.textContent = storedUserName.charAt(0).toUpperCase();
     } else {
-        // Если имя не найдено, можно установить значения по умолчанию
         userNameElement.textContent = 'Гость';
         userAvatarElement.textContent = 'Г';
     }
-
-    // Добавляем обработчик для кнопки "Добавить нового человека"
     const addPersonButton = document.querySelector('.add-person-button');
     if (addPersonButton) {
         addPersonButton.addEventListener('click', () => {
-            // Перенаправляем на страницу регистрации (или ввода данных нового человека)
-            // Предположим, что это register.html или input_form.html
-            window.location.href = 'register.html'; // Или 'input_form.html'
+            window.location.href = 'register.html';
         });
     }
-
-    // Здесь можно добавить другие обработчики событий для main.html, если потребуется
+    const analysisCard = document.getElementById('analysisCard');
+    if (analysisCard) {
+        analysisCard.addEventListener('click', () => {
+            window.location.href = 'analysis_page.html';
+        });
+    }
 });
