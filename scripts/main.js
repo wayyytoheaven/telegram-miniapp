e0f6c0ae79816b0c69cee4d3e6182041aa05a136
wayyytoheaven <133392697+wayyytoheaven@users.noreply.github.com>
2025-06-05 14:45:42 +0300

    Updated onboarding text and fixed button styles

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const userNameElement = document.getElementById('userName');
-    const userAvatarElement = document.getElementById('userAvatar');
-    const storedUserName = localStorage.getItem('userName');
-
-    if (storedUserName) {
-        userNameElement.textContent = storedUserName;
-        userAvatarElement.textContent = storedUserName.charAt(0).toUpperCase();
-    } else {
-        userNameElement.textContent = 'Гость';
-        userAvatarElement.textContent = 'Г';
-    }
-    const addPersonButton = document.querySelector('.add-person-button');
-    if (addPersonButton) {
-        addPersonButton.addEventListener('click', () => {
-            window.location.href = 'register.html';
-        });
-    }
     const analysisCard = document.getElementById('analysisCard');
+    const luckyDatesCard = document.getElementById('luckyDatesCard');
     if (analysisCard) {
         analysisCard.addEventListener('click', () => {
             window.location.href = 'analysis_page.html';
+        });
+    }
+    if (luckyDatesCard) {
+        luckyDatesCard.addEventListener('click', () => {
+            window.location.href = 'favorable_dates.html';
         });
     }
 });
